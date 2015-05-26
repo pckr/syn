45053a5e0a924d04edaeb7e4424b47be91489e61
pckr <rob@pckr.co.uk>
2015-05-26 10:55:24 +0000

    tidy

--- syn.py
+++ syn.py
@@ -7,6 +7,8 @@ import ipaddr
 
 not_valid = [10,127,169,172,192]
 
+
+
 for x in range(0,10):
 
         #generate random numbers between 1 and 256, but not RFC1918
@@ -24,10 +26,10 @@ for x in range(0,10):
         count = randrange(1,1000)
         print "Number of packets:",count
 
-        p=IP(dst=sys.argv[1],src=sip)/TCP(dport=int(sys.argv[2]),sport=RandShort                                                                                                                           (),flags="S")
+        p=IP(dst=sys.argv[1],src=sip)/TCP(dport=int(sys.argv[2]),sport=RandShort(),flags="S")
 
         #send/receive listener for p
-        ans,unans=srloop(p,inter=0.000001,count=count)
+        ans,unans=srloop(p,inter=0.01,count=count)
 
         #print summarys
         #ans.summary()
